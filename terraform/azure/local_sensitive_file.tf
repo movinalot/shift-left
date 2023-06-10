8aba181ba9bbb173834424322b24f29dc1caa6c0
John McDonough <movinalot@gmail.com>
2023-06-09 22:39:29 -0400

    update format

--- terraform/azure/local_sensitive_file.tf
+++ terraform/azure/local_sensitive_file.tf
@@ -1,13 +1,13 @@
 locals {
   fortigate_vars = format(
     "%s = \"%s\"\n%s = \"%s\"\n%s = \"%s\"\n%s = \"%s\"\n%s = \"%s\"\n%s = \"%s\"\n%s = { \"%s\" = { device = \"%s\", dst = \"%s\", gateway = \"%s\", status = \"%s\" } }",
-    "fortigate_api_token", random_string.string.id,
+    "fortigate_api_token ", random_string.string.id,
     "fortigate_ip_or_fqdn", azurerm_public_ip.public_ip["pip-fgt"].ip_address,
-    "resource_group_name", local.resource_group_name,
-    "route_table_name", azurerm_route_table.route_table["rt-protected"].name,
-    "next_hop_ip", azurerm_network_interface.network_interface["nic-fgt-port2"].private_ip_address,
-    "webhook", azurerm_automation_webhook.automation_webhook["Update-RouteTable_webhook"].uri,
-    "static_routes", "protected", "port2", azurerm_subnet.subnet["snet-protected"].address_prefixes[0], cidrhost(azurerm_subnet.subnet["snet-internal"].address_prefixes[0], 1), "enable"
+    "resource_group_name ", local.resource_group_name,
+    "route_table_name    ", azurerm_route_table.route_table["rt-protected"].name,
+    "next_hop_ip         ", azurerm_network_interface.network_interface["nic-fgt-port2"].private_ip_address,
+    "webhook             ", azurerm_automation_webhook.automation_webhook["Update-RouteTable_webhook"].uri,
+    "static_routes       ", "protected", "port2", azurerm_subnet.subnet["snet-protected"].address_prefixes[0], cidrhost(azurerm_subnet.subnet["snet-internal"].address_prefixes[0], 1), "enable"
   )
 }
 
